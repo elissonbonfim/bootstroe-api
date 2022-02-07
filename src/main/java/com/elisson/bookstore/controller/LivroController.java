b8775bce0acad62fbab5dbaec80a22ff905e9290
elissonbonfim <elisson100@outlook.com>
2022-02-06 21:10:33 -0300

    Crud livro

---
+++ src/main/java/com/elisson/bookstore/controller/LivroController.java
@@ -0,0 +1,67 @@
+package com.elisson.bookstore.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.elisson.bookstore.domain.Livro;
+import com.elisson.bookstore.dtos.LivroDTO;
+import com.elisson.bookstore.service.LivroService;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivroController {
+
+	@Autowired
+	private LivroService livroService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Livro> findById(@PathVariable Integer id){
+		Livro obj = livroService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = livroService.findAll(id_cat);
+		List<LivroDTO> listDto = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat, @RequestBody Livro obj){
+		Livro newObj = livroService.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		livroService.delete(id);
+		return ResponseEntity.noContent().build();
+	} 
+}
+
+
+
+
+
--- src/main/java/com/elisson/bookstore/service/LivroService.java
+++ src/main/java/com/elisson/bookstore/service/LivroService.java
@@ -1,12 +1,15 @@
 package com.elisson.bookstore.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.elisson.bookstore.repository.LivroRepository;
+import com.elisson.bookstore.domain.Categoria;
 import com.elisson.bookstore.domain.Livro;
+import com.elisson.bookstore.exceptions.ObjectNotFoundException;
 
 @Service
 public class LivroService {
@@ -14,8 +17,45 @@ public class LivroService {
 	@Autowired
 	private LivroRepository livroRepository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public List<Livro> findAll() {
 		return livroRepository.findAll();
+	}
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = livroRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return livroRepository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return livroRepository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNomeAutor(obj.getNomeAutor());
+		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return livroRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		livroRepository.delete(obj);
 	}
 
 }
